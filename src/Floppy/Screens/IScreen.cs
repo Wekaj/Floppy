94873c4ba3b6d6ddc3c2c59a6bc7e66104aecaec
Wekaj <wekkaj@gmail.com>
2020-04-13 12:12:38 +1200

    Added basic screen support.

---
+++ src/Floppy/Screens/IScreen.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Floppy.Screens {
+    public interface IScreen {
+        void Update(GameTime gameTime);
+        void Draw(GameTime gameTime);
+    }
+
+    public interface IScreen<T> : IScreen {
+        void Initialize(T args);
+    }
+}

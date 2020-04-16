0d6a263ab1782a43e76ebbefbbdb52e85650a3fa
Wekaj <wekkaj@gmail.com>
2020-04-16 19:33:33 +1200

    Added FloppyMath tests and comments.

--- src/Floppy/Utilities/FloppyMath.cs
+++ src/Floppy/Utilities/FloppyMath.cs
@@ -1,8 +1,17 @@
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using System;
 
 namespace Floppy.Utilities {
+    /// <summary>
+    /// Contains helpful math operations.
+    /// </summary>
     public static class FloppyMath {
+        /// <summary>
+        /// Create a unit vector for a provided angle in radians, where 0π translates to (1, 0), π/2 translates 
+        /// to (0, 1), π translates to (-1, 0), and 3π/2 translates to (0, -1).
+        /// </summary>
+        /// <param name="angle">The angle in radians.</param>
+        /// <returns>The unit vector.</returns>
         public static Vector2 VectorFromAngle(float angle) {
             return new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
         }
---
+++ test/Floppy.Tests/Utilities/FloppyMathTests.cs
@@ -0,0 +1,34 @@
+﻿using Floppy.Utilities;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Floppy.Tests.Utilities {
+    public class FloppyMathTests {
+        #region Test Cases
+
+        public static IEnumerable<object[]> VectorFromAngleCases { get; } = new[] {
+            new object[] { float.NaN, new Vector2(float.NaN, float.NaN) },
+            new object[] { float.PositiveInfinity, new Vector2(float.NaN, float.NaN) },
+            new object[] { float.NegativeInfinity, new Vector2(float.NaN, float.NaN) },
+            new object[] { 0f, new Vector2(1f, 0f) },
+            new object[] { 0.785f, new Vector2(0.707f, 0.707f) },
+            new object[] { 1.571f, new Vector2(0f, 1f) },
+            new object[] { 2.356f, new Vector2(-0.707f, 0.707f) },
+            new object[] { 3.142f, new Vector2(-1f, 0f) },
+            new object[] { 3.927f, new Vector2(-0.707f, -0.707f) },
+            new object[] { 4.712f, new Vector2(0f, -1f) },
+            new object[] { 5.498f, new Vector2(0.707f, -0.707f) },
+        };
+
+        #endregion
+
+        [Theory, MemberData(nameof(VectorFromAngleCases))]
+        public void VectorFromAngle_ValidAngle_CorrectUnitVector(float angle, Vector2 expectedVector) {
+            Vector2 actualVector = FloppyMath.VectorFromAngle(angle);
+
+            Assert.Equal(expectedVector.X, actualVector.X, 3);
+            Assert.Equal(expectedVector.Y, actualVector.Y, 3);
+        }
+    }
+}
